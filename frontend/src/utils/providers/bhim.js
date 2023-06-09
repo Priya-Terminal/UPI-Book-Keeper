ee4754a09eb2eda8f533f3df78878c9f8b78538c
Krushi Raj Tula <krushiraj123@gmail.com>
2023-06-10 01:01:59 +0530

    update bhim

--- frontend/src/utils/providers/bhim.js
+++ frontend/src/utils/providers/bhim.js
@@ -1,22 +1,23 @@
 export function extractBhimData(extractedText) {
   const transactions = [];
-  const bhimRegex = /(FAILED|SUCCESS).*?(?:(\d{4})|((\d{1,3},)?\d{1,3},\d{3})|(\d{3})).*?((?:Transaction|Tansaciion|Transacton) (?:ID|I|D|1D) .*? (\d+) .*?Remarks)/gis;
-  let match;
-  while ((match = bhimRegex.exec(extractedText)) !== null) {
-    console.log("Match found:", match);
+  const bhimRegex1 =
+    /(\w+)\sto\s?upi\s?id.*amount transact.+[1|i]D\s.([\d\.]+)\s(\d+)/i;
+  const bhimRegex2 = /(failed|success)[^\d]*([\d\.,]+).+\s(\w+)\s\w+\supi\s?[1i]?d.*transact.+\s(\d{5,})/i;
 
-    let transactionData = {
-      provider: 'bhim',
-      match: match[0],
-      transactionId: match[7],
-      transactionAmount: match[3] || match[5],
-      transactionStatus: match[1],
-      date: "Not Available"
-    };
-    transactions.push(transactionData);
+  let [match, status, amount, id] = extractedText.match(bhimRegex1) || [];
+
+  if (!match) {
+    [match, , amount, status, id] = extractedText.match(bhimRegex2) || [];
   }
-  console.log("Total transactions:", transactions.length);
-  return transactions;
-};
 
 
+  let transactionData = {
+    provider: "BHIM",
+    match,
+    id,
+    amount: parseFloat(amount.replace(/,/g, "")),
+    status,
+    date: Date.now(),
+  };
+  return transactionData;
+}
