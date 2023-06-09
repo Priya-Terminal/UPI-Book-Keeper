80c6f28f34f77b027ddee0e0bc1fa2ffb2f9d9b4
Krushi Raj Tula <krushiraj123@gmail.com>
2023-06-10 01:58:08 +0530

    add paytm

--- frontend/src/utils/providers/paytm.js
+++ frontend/src/utils/providers/paytm.js
@@ -1,25 +1,19 @@
 export function extractPaytmData(extractedText) {
-  const transactions = [];
-  const paytmRegex = /(Sent Successfully|Payment Failed|Transfer Pending|Transfer Failed|Money Received).*?(?<=Rupees)(.*)(?=Only).*?((\d{2}:\d{2} [AP]M, \d{2} [A-Z][a-z]+ \d{4})|(\d{2} [A-Z][a-z]+ \d{4}, \d{2}:\d{2} [AP]M)).*?((UPI Ref(?:erence)? No:\s(\d+))|(UPI Reference ID:\s([A-Za-z0-9]+)))/gis;
+  const paytmRegex =
+    /(success|failed|received)[^\d]*([\d,]+)\s.+(rupees .).*UPI\s?Ref\s?No:\s?(\d+)/i;
 
-  let match;
+  const [match, status, amount, testString, id] =
+    extractedText.match(paytmRegex);
 
-  while ((match = paytmRegex.exec(extractedText)) !== null) {
-    console.log("Match found:", match[0]);
-
-    let transactionData = {
-      provider: 'paytm',
-      match: match[0],
-      transactionId: match[6],
-      transactionAmount: match[2],
-      transactionStatus: match[1],
-      date: match[3]
-    };
-    transactions.push(transactionData);
-  }
-  console.log("Total transactions:", transactions.length);
-  return transactions;
-};
-
-
-
+  let transactionData = {
+    provider: "paytm",
+    match: match[0],
+    id,
+    amount: /t/i.test(testString)
+      ? parseFloat(amount.replace(/,/g, ""))
+      : parseFloat(amount.replace(/^3/, "").replace(/,/g, "")),
+    status,
+    date: Date.now(),
+  };
+  return transactionData;
+}
